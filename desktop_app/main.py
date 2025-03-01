92b15dcfd3ec53e18c83703a651ad60719c26c0f
Ryan bonavida <rbonavida@sram.com>
2025-03-01 09:12:08 -0700

    Add temporary print out in main to test

--- desktop_app/main.py
+++ desktop_app/main.py
@@ -1,5 +1,10 @@
+import pathlib
+
+from file_parser import parse_csv
 def main():
-    print("Hello from desktop-app!")
+    path_to_csv = pathlib.Path(input("path to csv file: "))
+    for item in parse_csv(path_to_csv):
+        print(item)
 
 
 if __name__ == "__main__":
