53403b1aa9465331a314b72ef42465fe03897365
Ryan bonavida <rbonavida@sram.com>
2025-03-03 20:23:39 -0700

    Add bike_record_summary endpoint

--- thundercloud/main.py
+++ thundercloud/main.py
@@ -14,6 +14,8 @@ def ingest_bike_record(ride_id: int, bike_record: BikeRecord):
     else:
         bike_records[ride_id].append(bike_record)
     print(f"Total bike records for id {ride_id}: {len(bike_records[ride_id])}")
+    # sort record
+    sorted(bike_records[ride_id], key=lambda x: x.timestamp)
     return {"message": f"Bike record received for {ride_id}"}
 
 @app.get("/bike_record/{ride_id}")
@@ -22,6 +24,19 @@ def get_bike_records(ride_id: int):
     if ride_id not in bike_records:
         raise HTTPException(status_code=404, detail=f"bike record {ride_id} not found")
     return bike_records.get(ride_id)
+
+@app.get("/bike_record/summary/{ride_id}")
+def get_bike_record_summary(ride_id: int):
+    print(f"Generating ride data  summary ")
+    ride_data = bike_records.get(ride_id)
+    if not ride_data:
+        raise HTTPException(status_code=404, detail=f"bike record {ride_id} not found")
+    
+    summary = RideSummary(start_time=ride_data[0].timestamp, 
+                          end_time=ride_data[-1].timestamp, 
+                          avg_speed=find_average_speed(ride_data), 
+                          ending_soc=ride_data[-1].soc)
+    return summary
 
 @app.get("/")
 def main():
